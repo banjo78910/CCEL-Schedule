50466857c3e1f81461020b6572baa299ee60c00d
Benjamin Ratner <banjo78910@usbadapt.STUDENT.CWRU.Edu>
2015-10-24 14:48:50 -0400

    add bootstrap, bootbox. Add basic login modal.

--- js/home.js
+++ js/home.js
@@ -1,8 +1,32 @@
 $( document ).ready( function() {
 
 	$( "#login" ).click( function() {
-		console.log( "click" );
-		$( "#login-form" ).slideToggle();
+		bootbox.dialog( {
+			title: "Login",
+			message: '<div class="row">  ' +
+				'<div class="col-md-12"> ' +
+				'<form class="form-horizontal"> ' +
+				'<div class="form-group"> ' +
+				'<label class="col-md-4 control-label" for="name">Username</label> ' +
+				'<div class="col-md-4"> ' +
+				'<input id="name" name="name" type="text" class="form-control input-md"> ' +
+				'</div> ' +
+				'<label class="col-md-4" control-label" for="password">Password</label>' +
+				'<div class="col-md-4">' +
+				'<input id="password" name="password" type="password" class="form-control form input-md">' +
+				'</div> ' +
+				'</div> </div>' +
+				'</form> </div>  </div>',
+			buttons: {
+				login: {
+					label: "Login",
+					className: "btn-primary",
+					callback: function() {
+
+					}
+				}
+			}
+		} );
 	} );
 
 } );
