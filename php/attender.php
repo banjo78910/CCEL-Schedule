04e65dea1c804038a790033be972a9734be521e4
asm115 <asm115@case.edu>
2015-12-02 19:16:51 -0500

    Update attender.php

--- php/attender.php
+++ php/attender.php
@@ -1,26 +1,29 @@
 <?php
 class Attender {
 	private $connection;
-	private $sessions;
 	private $username;
 	
 	public function __construct($username, $connection) {
 		$this->connection = $connection;
-		$this->sessions = null;
 		$this->username = $username;
 	}
 	
-	public function retrieveAttendingSessions() {
-		$narrowQuery = "select sessionID from {$GLOBALS["database"]}.willAttend where userID = '{$this->username}'";
-		$query = "select * from {$GLOBALS["database"]}.session where sessionID in ({$narrowQuery})";
-		$this->sessions = $this->connection->query($query);
-		while ($row = $this->sessions->fetch_assoc()) {
-			mediator::displaySession($row);
+	public function displayAttendingSessions() {
+		$query = "select sessionID from {$GLOBALS["database"]}.willAttend where userID = '{$this->username}'";
+		$sessions = $this->connection->query($query);
+		while ($row = $sessions->fetch_assoc()) {
+			$session = new Session($row['sessionID']);
+			$session->display();
 		}
 	}
 	
 	public function willAttend($sessionID) {
 		$query = "insert into {$GLOBALS["database"]}.willAttend values ({$sessionID}, '{$this->username}')";
+		$this->connection->query($query);
+	}
+	
+	public function cancelAttend($sessionID) {
+		$query = "delete from {$GLOBALS["database"]}.willAttend where sessionID = $sessionID and userID = '{$this->username}'";
 		$this->connection->query($query);
 	}
 }
